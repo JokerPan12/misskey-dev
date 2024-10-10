d746abf777f2aa986ea61590c389c758f0adca45
JokerPan <60731479+JokerPan12@users.noreply.github.com>
2024-10-10 12:58:47 +0800

    Update MkVisitorDashboard.vue

--- packages/frontend/src/components/MkVisitorDashboard.vue
+++ packages/frontend/src/components/MkVisitorDashboard.vue
@@ -23,7 +23,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 			</div>
 			<div class="_gaps_s" :class="$style.mainActions">
 				<MkButton :class="$style.mainAction" full rounded gradate data-cy-signup style="margin-right: 12px;" @click="signup()">{{ i18n.ts.joinThisServer }}</MkButton>
-				<MkButton :class="$style.mainAction" full rounded link to="https://misskey-hub.net/servers/">{{ i18n.ts.exploreOtherServers }}</MkButton>
 				<MkButton :class="$style.mainAction" full rounded data-cy-signin @click="signin()">{{ i18n.ts.login }}</MkButton>
 			</div>
 		</div>
@@ -46,6 +45,9 @@ SPDX-License-Identifier: AGPL-3.0-only
 	</div>
 	<div :class="$style.panel">
 		<XActiveUsersChart/>
+	</div>
+	<div style="text-align: center; position: absolute">
+		<a href="https://beian.miit.gov.cn/" target="_blank">鄂ICP备2023017034号-2</a>
 	</div>
 </div>
 </template>
